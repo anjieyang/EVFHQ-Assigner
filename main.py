1d81c1a97972ff591645df0c3ced17bbf250639c
Anjie Yang <digmouse233@gmail.com>
2024-04-16 21:37:49 -0400

    Pass resolution to downloader

--- main.py
+++ main.py
@@ -27,14 +27,19 @@ def setup_rabbitmq_connection():
 def fetch_unassigned_video(conn):
     with conn.cursor() as cur:
         cur.execute(
-            "SELECT video_id FROM videos WHERE location IS NULL AND server_id IS NULL LIMIT 1;")
+            "SELECT video_id, resolution FROM videos WHERE location IS NULL AND server_id IS NULL LIMIT 1;")
         row = cur.fetchone()
-        return row[0] if row else None
+        return row if row else None
 
 
-def assign_task_to_downloader(video_id, channel, conn, server_id):
+def assign_task_to_downloader(video_id, resolution, channel, conn, server_id):
     with conn.cursor() as cur:
-        message = json.dumps({'video_id': video_id, 'server_id': server_id})
+        message = json.dumps({
+            'video_id': video_id,
+            'resolution': resolution,
+            'server_id': server_id
+        })
+        print(message)
         channel.basic_publish(
             exchange='',
             routing_key='video_download_queue',
@@ -42,17 +47,19 @@ def assign_task_to_downloader(video_id, channel, conn, server_id):
             properties=pika.BasicProperties(delivery_mode=2)
         )
         cur.execute(
-            "UPDATE videos SET server_id = %s WHERE video_id = %s;", (-1, video_id))
+            "UPDATE videos SET server_id = %s WHERE video_id = %s;", (server_id, video_id))
         conn.commit()
-        print(f"Task for video_id {video_id} assigned to server.")
+        print(f"Task for video_id {video_id} assigned to server {server_id}.")
 
 
 def handle_task_request(channel, method, properties, body):
     server_id = json.loads(body.decode())['server_id']
     conn = setup_database_connection()
-    video_id = fetch_unassigned_video(conn)
-    if video_id:
-        assign_task_to_downloader(video_id, channel, conn, server_id)
+    result = fetch_unassigned_video(conn)
+    if result:
+        video_id, resolution = result
+        assign_task_to_downloader(
+            video_id, resolution, channel, conn, server_id)
     else:
         print("No videos left to process.")
     channel.basic_ack(delivery_tag=method.delivery_tag)
@@ -62,25 +69,20 @@ def update_video_status(channel, method, properties, body):
     data = json.loads(body.decode())
     conn = setup_database_connection()
     with conn.cursor() as cur:
-        if data['status_code'] == 0:
-            cur.execute(
-                "UPDATE videos SET location = %s, server_id = %s WHERE video_id = %s;",
-                (data['location'], data['server_id'], data['video_id']))
-        else:
-            cur.execute(
-                "UPDATE videos SET location = %s, server_id = %s WHERE video_id = %s;",
-                ("FAILED", data['server_id'], data['video_id']))
+        cur.execute(
+            "UPDATE videos SET location = %s, server_id = %s WHERE video_id = %s;",
+            (data['location'], data['server_id'], data['video_id']) if data['status_code'] == 0 else ("FAILED", data['server_id'], data['video_id']))
         conn.commit()
-    print(
-        f"Video {data['video_id']} processed {'SUCCESSFULLY' if data['status_code'] == 0 else 'FAILED'} by server {data['server_id']}.")
+        print(
+            f"Video {data['video_id']} processed {'SUCCESSFULLY' if data['status_code'] == 0 else 'FAILED'} by server {data['server_id']}.")
 
-    video_id = fetch_unassigned_video(conn)
-    server_id = data['server_id']
-    if video_id:
-        assign_task_to_downloader(video_id, channel, conn, server_id)
+    result = fetch_unassigned_video(conn)
+    if result:
+        video_id, resolution = result
+        assign_task_to_downloader(
+            video_id, resolution, channel, conn, data['server_id'])
     else:
         print("No videos left to process.")
-
     channel.basic_ack(delivery_tag=method.delivery_tag)
 
 
